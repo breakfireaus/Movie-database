15026385b6bcfecafc26fdffffc8dd7a13739f7c
Miles Thomas <milesman2013@gmail.com>
2022-09-22 19:24:49 +1000

    Adding results.js

---
+++ assets/JS/results.js
@@ -0,0 +1,36 @@
+
+
+// API keys
+var movieAPI = 'https://api.themoviedb.org?api_key=f773dd7be92f1943bb6b98b40e74c3bf'
+var musicAPI = 'https://api.spotify.com'
+
+
+// Modal JS 
+// Get DOM Elements
+const modal = document.querySelector('#result-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('#close-modal');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
+// end of modal script 
