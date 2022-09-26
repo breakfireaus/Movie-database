8339ddeba77d921a784a37aafb571bfb13b116cb
rachael-pluta <rachael.pluta.work@gmail.com>
2022-09-27 00:31:08 +1000

    created query string from user search input

--- assets/JS/results.js
+++ assets/JS/results.js
@@ -1,37 +1,8 @@
-// global variables
-
-
-
+// Declare global variables
 var searchButton = document.querySelector('#search-button')
 
-
-
-// APIs
-
-
-
+// API keys
 var musicAPIkey = '327d3bf7241329fd83a0889ff32d9943'
-
-//previous search history appears on page and persistent(local storage)
-// function
-// search bar 
-// and button to save to local storage
-// retrieve from local Storage
-// creating the list items
-
-//search results appear on page
-// function
-// pull the search the parameters
-// search the movie api function includes the fetch for title
-// if no results found else results pop
-
-//when the result is clicked it appears in the modal with an image of the movie and list of soundtracks
-// function to pull the image of the movie that was clicked
-// function to pull from music database api to show soundtrack File
-// append into modal
-
-
-
 
 // Modal JS 
 // Get DOM Elements
@@ -60,13 +31,42 @@ function outsideClick(e) {
     modal.style.display = 'none';
   }
 }
-
-
 // end of modal script 
 
-//obtain music tracks
+/* Gets the search parameters from the home page and previous history page
+function getParams() {
+  var searchParamsArr = document.location.search;
+  var movieResults = searchParamsArr[0].split("=").pop();
 
+  searchResults(movieResults);
+}
 
+// Searches the movie API using the search parameters
+function searchResults(movieResults) {
+  this.preventDefault();
+  var movieQueryUrl = "https://api.themoviedb.org/3/search/movie?api_key=f773dd7be92f1943bb6b98b40e74c3bf&query=" + movieResults;
+
+  fetch(movieQueryUrl)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.results.length; i++) {
+        var resultsCard = document.createElement("button");
+        resultsCard.onlick = resultsModalDisplay;
+        resultsCard.innerHTML = data.results[i].original_title;
+        document.querySelector("#results").append(resultsCard);
+      }
+      function resultsModalDisplay() {
+        document.querySelector('#movie-title-filled').textContent = data[i].original_title;
+        document.querySelector('#image').src = data[i].poster_path;
+      }
+    })
+} */
+
+// Searches for the movie using the search form on the results page
 searchButton.addEventListener('click', searchResults)
 
 function searchResults(e) {
@@ -74,15 +74,15 @@ function searchResults(e) {
   var searchInput = document.querySelector('input[name="movie-search"]')
   var searchInputVal = searchInput.value;
   var movieAPIkey = 'https://api.themoviedb.org/3/search/movie?api_key=f773dd7be92f1943bb6b98b40e74c3bf&query=' + searchInputVal;
-   
-    fetch(movieAPIkey)
+
+  fetch(movieAPIkey)
     .then(function (response) {
       console.log(response);
       return response.json();
-    }) 
+    })
     .then(function (data) {
       console.log(data);
-      for (var i=0; i < data.results.length; i++) {
+      for (var i = 0; i < data.results.length; i++) {
         var resultsCard = document.createElement('button');
         resultsCard.onclick = resultsModalDisplay;
         resultsCard.innerHTML = data.results[i].original_title;
@@ -93,39 +93,34 @@ function searchResults(e) {
         document.querySelector('#movie-title-filled').textContent = data[i].original_title;
         document.querySelector('#image').src = data[i].poster_path;
       }
-
     });
 }
 
 function playlistPull() {
   fetch('https://api.deezer.com/search/album?q=' + resultsCard.innerHTML + 'soundtrack&appid=' + musicAPIkey)
-  .then (function (response) {
-    if (response.status === 404) {
-      console.log('No soundtrack could be found')
-    } else {
-      return response.json()
-    }
-  })
-  .then (function (data) {
-    console.log(data)
-    var searchID = data.id
-
-    fetch('https://api.deezer.com/album/' + searchID)
     .then(function (response) {
-      return response.json()
+      if (response.status === 404) {
+        console.log('No soundtrack could be found')
+      } else {
+        return response.json()
+      }
     })
     .then(function (data) {
       console.log(data)
-      for (var i = 0; i < data.tracks.length; i++) {
-        var albumTrack = document.createElement(li)
-        albumTrack[i].textContent = data.tracks[i].title + ' by ' + data.tracks[i].artist.name 
-        albumTrack.append(document.querySelector('#music-list'))
-      }
+      var searchID = data.id
+
+      fetch('https://api.deezer.com/album/' + searchID)
+        .then(function (response) {
+          return response.json()
+        })
+        .then(function (data) {
+          console.log(data)
+          for (var i = 0; i < data.tracks.length; i++) {
+            var albumTrack = document.createElement(li)
+            albumTrack[i].textContent = data.tracks[i].title + ' by ' + data.tracks[i].artist.name
+            albumTrack.append(document.querySelector('#music-list'))
+          }
+        })
     })
-  })
 }
 
-
-
-
-// image to display in modal
