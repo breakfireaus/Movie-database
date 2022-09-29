4b109e0cd033680febb1b08822843ee507069961
Miles Thomas <milesman2013@gmail.com>
2022-09-29 12:38:14 +1000

    annotated pages

--- assets/JS/results.js
+++ assets/JS/results.js
@@ -5,7 +5,7 @@ var searchButton = document.querySelector('#search-button');
 var searchInput;
 var searchID;
 
-
+//Provides current day and time to all pages
 function currentMomentDate() {
   currentDate = dayjs().format("dddd hh:mm A");
   currentDateEl.text(currentDate);
@@ -39,12 +39,13 @@ function outsideClick(e) {
 }
 // end of modal script 
 
-
+//Array used to store information from searched movies
 let resultsArray = [];
 
-// Searches the movie API for the movie title
+// Adds event listener to search buttons
 searchButton.addEventListener("click", searchResults);
 
+//Determines movie title search parameters
 let movieSearchParams = false
 
 function searchResults(event) {
@@ -62,6 +63,8 @@ function searchResults(event) {
     movieSearchParams = false;
     searchInputVal = document.location.search.split("=").pop();
   }
+  
+  // Link used to search the first movie API
   var movieAPIkey = "https://api.themoviedb.org/3/search/movie?api_key=f773dd7be92f1943bb6b98b40e74c3bf&query=" + searchInputVal;
 
   document.querySelector('#search-display').textContent = searchInput.value;
@@ -71,6 +74,7 @@ function searchResults(event) {
     .then(function (response) {
       return response.json();
     })
+    //Pulls the first five search results from the array fetched and displays them on the page
     .then(function (data) {
       resultsArray = [...data.results]
       console.log(resultsArray)
@@ -84,6 +88,7 @@ function searchResults(event) {
           resultsCard.classList.add('search-results');
         }
 
+        //Displays detailed movie information in the modal based on the result selected 
         function resultsModalDisplay() {
           modal.style.display = "block";
           var modalTitle = document.querySelector("#movie-title-filled");
@@ -98,6 +103,7 @@ function searchResults(event) {
           modalImage.src = 'https://image.tmdb.org/t/p/original/' + image;
           secondSearch();
 
+          //Fetches a second movie API for additional movie information on the modal
           function secondSearch() {
             var OMDBAPILink = 'http://www.omdbapi.com/?apikey=406ff228&t=' + modalTitle.textContent;
             fetch(OMDBAPILink)
@@ -114,7 +120,6 @@ function searchResults(event) {
                 var criticRatings = document.createElement('li')
                 criticRatings.textContent = data.Ratings[i].Source + ": " + data.Ratings[i].Value
                 document.querySelector('#movie-details').append(criticRatings)
-                criticRatings.classList.add('text-left')
               }
             })
           }
@@ -122,11 +127,13 @@ function searchResults(event) {
     });
 }
 
+//Clears the page of search results
 var clearHistoryBtn = document.querySelector("#clear-results");
 clearHistoryBtn.addEventListener("click", function() {
   document.querySelector("#results").innerHTML = "";
 })
 
+//Searches for a query string from home page and previous results 
 if (document.location.search.length > 0) {
   movieSearchParams = true
   searchResults();
